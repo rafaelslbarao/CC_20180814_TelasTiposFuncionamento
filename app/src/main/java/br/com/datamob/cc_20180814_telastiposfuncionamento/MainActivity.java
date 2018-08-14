cff2517595fdb9512407e0996b20dc112b8c04cb
Datamob Informática <Datamob@MacBook-Air-de-Datamob.local>
2018-08-14 18:30:15 -0300

    Initial commit

---
+++ app/src/main/java/br/com/datamob/cc_20180814_telastiposfuncionamento/MainActivity.java
@@ -0,0 +1,15 @@
+package br.com.datamob.cc_20180814_telastiposfuncionamento;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
